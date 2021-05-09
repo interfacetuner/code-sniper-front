cf3bf8b5b5933a53144db57e339b6b72019bd6c3
Damian Hankins <damianchankins@gmail.com>
2021-05-09 12:37:43 -0400

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = (props) => (
             <img src={props.user.photoURL} alt={props.user.displayName} />
           </li>
           
-          <li class="w-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={logout}>
+          <li class="w-20 text-white font-bold py-2 px-4 rounded bg-blue-500 hover:bg-red-700 ..." onClick={logout}>
             Logout
           </li>
          
@@ -21,7 +21,7 @@ const Header = (props) => (
 
         </>
       ) : (
-        <li class="w-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={login}>
+        <li class="w-20 text-white font-bold py-2 px-4 rounded bg-blue-500 hover:bg-red-700 ..." onClick={login}>
           Login
         </li>
       )}
